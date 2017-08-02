e5c7f1b89dc13210911966bdab89fb1d0c0b289a
AndrewSerbin <andrewserbin01@gmail.com>
2017-08-02 21:55:23 +0300

    Added builder setters.

--- src/main/java/model/entity/builder/RecordBuilder.java
+++ src/main/java/model/entity/builder/RecordBuilder.java
@@ -22,25 +22,64 @@ class RecordBuilder {
     private Date dateOfEntryInNoteBook;
     private Date dateOfLastChanging;
 	
-	RecordBuilder setMake(String make) {
-		this.make = make;
-		return this;
-	}
-	RecordBuilder setTransmission(Transmission transmission) {
-		this.transmission = transmission;
-		return this;
-	}
-	RecordBuilder setMaxSpeed(int maxSpeed) {
-		this.maxSpeed = maxSpeed;
+	public RecordBuilder setFullName(String name, String surname,  String patronymic, String truncatedName) {
+		fullName = new FullName(surname, name, patronymic, truncatedName);
 		return this;
 	}
 	
-	Record build() {
+	public RecordBuilder setNick(String nick) {
+		this.nick = nick;
+		return this;
+	}
+	
+	public RecordBuilder setDesription(String description) {
+		this.description = description;
+		return this;
+	}
+	
+	public RecordBuilder setGroup(String group) {
+        this.group = Group.valueOf(group);
+        return this;
+    }
+	
+    public RecordBuilder setTelephone(String mainTelephone, String spareTelephone) {
+        this.telephone = new Telephone(mainTelephone, spareTelephone);
+        return this;
+    }
+    
+    public RecordBuilder setSkype(String skype) {
+        this.skype = skype;
+        return this;
+    }
+    
+    public RecordBuilder setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+    
+    public RecordBuilder setAddress(String index, String city, String street, String houseNumber, 
+    		String apartmentNumber, String fullAddress) {
+        this.address = new Address(index, city, street, houseNumber, apartmentNumber, fullAddress);
+        return this;
+    }
+    
+    public RecordBuilder setDateOfLastChanging(String dateOfLastChanging) {
+        this.dateOfLastChanging = new Date(dateOfLastChanging);
+        return this;
+    }
+	
+	public Record build() {
 		Record record = new Record();
-		car.setMake(make);
-		car.setTransmission(transmission);
-		car.setMaxSpeed(maxSpeed);
+		record.setFullName(fullName);
+		record.setNick(nick);
+		record.setDescription(description);
+		record.setGroup(group);
+		record.setTelephone(telephone);
+		record.setEmail(email);
+		record.setSkype(skype);
+		record.setAddress(address);
+		record.setDateOfLastChanging(dateOfLastChanging);
 		
-		return car;
+		return record;
 	}
 }
