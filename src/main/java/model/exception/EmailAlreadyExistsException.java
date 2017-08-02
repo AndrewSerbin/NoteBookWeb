5506d7e9924b34c08a35c35cdf684f41dc5fc4f8
AndrewSerbin <andrewserbin01@gmail.com>
2017-08-02 21:03:14 +0300

    Added exception.

---
+++ src/main/java/model/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,21 @@
+package model.exception;
+
+import model.entity.Record;
+
+public class EmailAlreadyExistsException extends Exception {
+
+    private Record record;
+
+    public EmailAlreadyExistsException(Record record) {
+        this.record = record;
+    }
+
+    public EmailAlreadyExistsException(String message, Record record) {
+        super(message);
+        this.record = record;
+    }
+
+    public Record getRecord() {
+        return record;
+    }
+}
