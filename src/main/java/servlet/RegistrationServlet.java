2f1c672c9b6a7777fd888ce9fce998066a6dfc0f
AndrewSerbin <andrewserbin01@gmail.com>
2017-08-02 23:06:15 +0300

    Added registration form.

--- src/main/java/servlet/RegistrationServlet.java
+++ src/main/java/servlet/RegistrationServlet.java
@@ -8,8 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/LoginServlet")
-public class LoginServlet extends HttpServlet {
+@WebServlet("/RegistrationServlet")
+public class RegistrationServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
        
@@ -17,16 +17,14 @@ public class LoginServlet extends HttpServlet {
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		
-		String login = request.getParameter("login");
-		String password = request.getParameter("password");
+		String name = request.getParameter("name");
 		
 		response.setContentType("text/html;charset=utf-8");
 		
 		PrintWriter out = response.getWriter();
 		
 		out.println("<h3>Профиль юзера</h3>");
-		out.println("Логин: " + login + "<br>");
-		out.println("Пароль: " + password + "<br>");
+		out.println("Name: " + name + "<br>");
 		out.close();
 	}
 
