f7dc5da6ef6e8d6e118f6ea9847f26dfd30865b6
AndrewSerbin <andrewserbin01@gmail.com>
2017-08-02 23:11:33 +0300

    Added setters and getters.

--- src/main/java/model/entity/Record.java
+++ src/main/java/model/entity/Record.java
@@ -15,144 +15,65 @@ public class Record {
 
     private Date dateOfEntryInNoteBook;
     private Date dateOfLastChanging;
-
-    public static Builder getBuilder() {
-        return new Record().new Builder();
-    }
-
-    public class Builder {
-
-        public Builder setFullName(FullName fullName) {
-            Record.this.fullName = fullName;
-            return this;
-        }
-
-        public Builder setNick(String nick) {
-            Record.this.nick = nick;
-            return this;
-        }
-
-        public Builder setDescription(String description) {
-            Record.this.description = description;
-            return this;
-        }
-
-        public Builder setGroup(Group group) {
-            Record.this.group = group;
-            return this;
-        }
-
-        public Builder setTelephone(Telephone telephone) {
-            Record.this.telephone = telephone;
-            return this;
-        }
-
-        public Builder setSkype(String skype) {
-            Record.this.skype = skype;
-            return this;
-        }
-
-        public Builder setEmail(String email) {
-            Record.this.email = email;
-            return this;
-        }
-
-        public Builder setAddress(Address address) {
-            Record.this.address = address;
-            return this;
-        }
-
-        public Builder setDateOfLastChanging(Date dateOfLastChanging) {
-            Record.this.dateOfLastChanging = dateOfLastChanging;
-            return this;
-        }
-
-        public Record build() {
-            return Record.this;
-        }
-    }
-
-    public void refreshDateOfEntryInNoteBook() {
-        dateOfEntryInNoteBook = new Date();
-    }
-
-    public FullName getFullName() {
-        return fullName;
-    }
-
-    public String getNick() {
-        return nick;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Group getGroup() {
-        return group;
-    }
-
-    public Telephone getTelephone() {
-        return telephone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getSkype() {
-        return skype;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public Date getDateOfEntryInNoteBook() {
-        return dateOfEntryInNoteBook;
-    }
-
-    public Date getDateOfLastChanging() {
-        return dateOfLastChanging;
-    }
-
-    public void setFullName(FullName fullName) {
-        this.fullName = fullName;
-    }
-
-    public void setNick(String nick) {
-        this.nick = nick;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setGroup(Group group) {
-        this.group = group;
-    }
-
-    public void setTelephone(Telephone telephone) {
-        this.telephone = telephone;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setSkype(String skype) {
-        this.skype = skype;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public void setDateOfEntryInNoteBook(Date dateOfEntryInNoteBook) {
-        this.dateOfEntryInNoteBook = dateOfEntryInNoteBook;
-    }
-
-    public void setDateOfLastChanging(Date dateOfLastChanging) {
-        this.dateOfLastChanging = dateOfLastChanging;
-    }
+    
+	public FullName getFullName() {
+		return fullName;
+	}
+	public void setFullName(FullName fullName) {
+		this.fullName = fullName;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Group getGroup() {
+		return group;
+	}
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+	public Telephone getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(Telephone telephone) {
+		this.telephone = telephone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSkype() {
+		return skype;
+	}
+	public void setSkype(String skype) {
+		this.skype = skype;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public Date getDateOfEntryInNoteBook() {
+		return dateOfEntryInNoteBook;
+	}
+	public void setDateOfEntryInNoteBook(Date dateOfEntryInNoteBook) {
+		this.dateOfEntryInNoteBook = dateOfEntryInNoteBook;
+	}
+	public Date getDateOfLastChanging() {
+		return dateOfLastChanging;
+	}
+	public void setDateOfLastChanging(Date dateOfLastChanging) {
+		this.dateOfLastChanging = dateOfLastChanging;
+	}
 }
