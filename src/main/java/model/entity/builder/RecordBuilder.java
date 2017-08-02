6d2b5380447d6597eb6b3c81eb5bcda626abaaf5
AndrewSerbin <andrewserbin01@gmail.com>
2017-08-02 23:34:18 +0300

    Changed indents.

--- src/main/java/model/entity/builder/RecordBuilder.java
+++ src/main/java/model/entity/builder/RecordBuilder.java
@@ -8,7 +8,7 @@ import model.entity.Record;
 import model.entity.Telephone;
 
 class RecordBuilder {
-	private FullName fullName;
+    private FullName fullName;
     private String nick;
     private String description;
     private Group group;
